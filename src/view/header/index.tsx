c037470cf320fc20c2d0259c7bbd32752fa9ef81
talhacheema675 <125282171+talhacheema675@users.noreply.github.com>
2023-06-22 03:37:46 +0500

    Update index.tsx

--- src/view/header/index.tsx
+++ src/view/header/index.tsx
@@ -9,7 +9,7 @@ import { Input } from "@/component/ui/input"
   const Header=()=>
   {
     return(
-        <nav className="flex flex-row lg:px-20 md:px-20 sm:px-2 px-2 pt-8 lg:gap-28 md:gap-28 sm:gap-4 gap-4 w-full bg-white fixed flex-wrap ">
+        <nav className="flex flex-row lg:px-20 md:px-20 sm:px-2 px-2 pt-8 lg:gap-28 md:gap-28 sm:gap-4 gap-4 w-full bg-white fixed  ">
             <div className="py-4"> <Link href="#"><Image src="/logo.webp" alt="Logo Image" width={130} height={130}/></Link></div>
             <div className="flex lg:text-base md:text-base text-xs lg:font-bold md:font-bold w-max "> <NavigationMenu>
                     <NavigationMenuList >
