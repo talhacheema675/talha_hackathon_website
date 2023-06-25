7b6d8daabfa21680d31e7fdebaad1caef536501a
talhacheema675 <125282171+talhacheema675@users.noreply.github.com>
2023-06-25 19:36:26 +0500

    Update page.tsx

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -13,16 +13,17 @@ const Details=({params}:{params:{slug:number}})=>
                     <div key={index} >
                             <div className="flex lg:flex-row md:flex-row sm:flex-col flex-col gap-8 flex-wrap "> 
                               <div> <Image src={item.image} alt="image" width="100" height={200} className="rounded-2xl lg:visible md:visible sm:invisible invisible"/></div>
-                              <div> <Image src={item.image} alt="image" width="500" height={1200} className="rounded-2xl"/></div>
-                              <div className="pt-10">
+                              <div className="lg:w-1/2 md:w-1/2 sm:w-full w-full h-96"> <Image src={item.image} alt="image" width="500" height={1200} className="rounded-2xl w-full h-full"/></div>
+                              <div className="pt-10 flex flex-col">
                                 <p className="text-3xl "> {item.name}</p>
                                 <p className="text-gray-500 text-2xl font-bold mb-10">{item.cloth}</p>
                                 <p className="text-lg font-bold mb-">select size</p>
-                                <Button className="rounded-full bg-slate-300 text-black mr-2">XS</Button>
+                                <div className="flex flex-row"><Button className="rounded-full bg-slate-300 text-black mr-2">XS</Button>
                                 <Button className="rounded-full bg-slate-300 text-black mr-2">S</Button>
                                 <Button className="rounded-full bg-slate-300 text-black mr-2">M</Button>
                                 <Button className="rounded-full bg-slate-300 text-black mr-2">L</Button>
                                 <Button className="rounded-full bg-slate-300 text-black mr-2 mb-8">XL</Button>
+                                </div>
                                 <p className="font-bold mb-8">Quantity <input type="text" className="w-10  border-solid border-black border-2"/></p>
                                 <p className="font-bold mb-8">Total : {item.price}</p>
                                 <Button className="w-44"><ShoppingCart/> Add to Cart </Button>
