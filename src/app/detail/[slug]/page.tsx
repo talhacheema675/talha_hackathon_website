dcaa97d25c21d77a1f08c3787aa970ce7372ba8c
talhacheema675 <125282171+talhacheema675@users.noreply.github.com>
2023-06-25 21:26:33 +0500

    Update page.tsx

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -12,9 +12,13 @@ const Details=({params}:{params:{slug:number}})=>
                 return(
                     <div key={index} >
                             <div className="flex lg:flex-row md:flex-row flex-col "> 
-                            <div className="flex flex-row lg:w-2/3 md:w-2/3 w-full gap-2 lg:m-6 md:m-6 m-1 md:px-8 border border-solid border-black">
-                              <div className="h-max w-max "> <Image src={item.image} alt="image" width="100" height={200} className="rounded-2xl  "/></div>
-                              <div className=" w-full"> <Image src={item.image} alt="image" width="500" height="1200" className="rounded-2xl w-full h-full"/></div>
+                            <div className="flex flex-row lg:w-2/3 md:w-2/3 w-full md:h-max h-96  gap-2 lg:m-6 md:m-6 m-1 ">
+                              <div className="flex flex-col gap-2"> 
+                                  <Image src={item.image} alt="image" width="100" height={200} className="rounded-2xl cursor-pointer "/>
+                                  <Image src={item.image} alt="image" width="100" height={200} className="rounded-2xl  cursor-pointer"/>
+                                  <Image src={item.image} alt="image" width="100" height={200} className="rounded-2xl  cursor-pointer"/>
+                              </div>
+                              <div className=" w-full h-full"> <Image src={item.image} alt="image" width="500" height="1200" className="rounded-2xl w-full h-full"/></div>
                               </div>
                               <div className="flex flex-col pt-16">
                                 <p className="text-3xl "> {item.name}</p>
@@ -26,7 +30,7 @@ const Details=({params}:{params:{slug:number}})=>
                                 <Button className="rounded-full bg-slate-300 text-black mr-2">L</Button>
                                 <Button className="rounded-full bg-slate-300 text-black mr-2 mb-8">XL</Button>
                                 </div>
-                                <p className="font-bold mb-8">Quantity <input type="text" className="w-10  border-solid border-black border-2"/></p>
+                                <p className="font-bold mb-8">Quantity <input type="text" className="w-10 border-2 border-solid border-black "/></p>
                                 <p className="font-bold mb-8">Total : {item.price}</p>
                                 <Button className="w-44"><ShoppingCart/> Add to Cart </Button>
 
